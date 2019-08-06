4bf3c5fdae0158bb1dacddfa642f309f31135074
Agasthian <agasthian_r@hotmail.com>
2019-08-06 15:41:08 +0530

    Added header section with react-router and compeleted the styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './components/pages/homepage/homepage.component';
 import Shop from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shop} />
