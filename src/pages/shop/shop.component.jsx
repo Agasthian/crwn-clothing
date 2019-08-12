e6852c8904b26d7da702d6f89500ffc5bcd6dac1
Agasthian <agasthian_r@hotmail.com>
2019-08-12 21:10:48 +0530

    Moved the data from directory state and collection state i.e shop data into redux. Created a collection overview component under shop component which allows shop component to nested route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop-data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class Shop extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = () => {
+  return (
+    <div className='shop-page'>
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default Shop;
