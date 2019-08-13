f996c2e767af84abfc06d24cf0e0e98e48265fd2
Agasthian <agasthian_r@hotmail.com>
2019-08-13 21:22:53 +0530

    Moved our directory and shop data into its own reducers, and create respective selectors and updated corresponding components with new redux floq for directory/shop

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 export const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
